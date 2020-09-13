945365b46672af6278ce46db118094c0135a1c38
Vladimir Trachenko <trachenko@gmail.com>
2020-09-13 21:34:13 +0300

    Add card and deck classes

---
+++ card.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative 'resource/card_constants'
+
+# Card class
+class Card
+  attr_accessor :suit, :rank, :value
+
+  def initialize(suit:, rank:, value:)
+    validate!(suit, rank)
+    @suit = suit
+    @rank = rank
+    @value = value
+  end
+
+  def ==(card)
+    suit == card.suit && rank == card.rank
+  end
+
+  private
+
+  def validate!(suit, rank)
+    valid = SUITS.include?(suit) && RANKS.include?(rank)
+    raise 'invalid suit or rank' unless valid
+  end
+end
---
+++ deck.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative 'card'
+require_relative 'resource/card_constants'
+
+# Card class
+class Deck
+  attr_reader :cards
+
+  def initialize
+    @cards = []
+    SUITS.each do |suit|
+      RANK_WITH_VALUE.each do |rank, value|
+        self.cards << Card.new(suit: suit, rank: rank, value: value)
+      end
+    end
+  end
+
+  def shuffle
+    cards.shuffle
+  end
+
+  def pop
+    cards.pop unless cards.empty?
+  end  
+end
